e2551bb44d79392631408dc0ab9417f1f6b65a27
williamd <willy1divx@yahoo.com>
2017-02-16 00:45:39 +0000

    new version check for password length and user ID redundancy

--- js/app.js
+++ js/app.js
@@ -1,80 +1,28 @@
-
-
-
-
-
-		
-		// // function getAnswer(){
-		// // 	var answer = document.getElementById('answer').value;
-
-		// // 		document.getElementById('userAnswer').textContent = answer;
-		// // 	}
-
-		// var numbers = [1,2,3,4,5];
-
-		
-  //       var biggest = numbers[0];
-		// // for (var i = 0; i < numbers.length ; i++){
-		// // 	if (numbers[i] > biggest){
-		// // 		biggest = numbers[i];
-		// // 	}
-		// // }
-		// // console.log(biggest);
-
-		// numbers.forEach(function(item){
-		// 	if (item > biggest){
-		// 		biggest = item;
-		// 		return;
-		// 	}
-		// 	return;
-		// });
-		// console.log(biggest);
-		
-// var mustang = {
-// 	make : "chevy",
-// 	year : "1967",
-// 	purchased: false
-// }
-
-// var fiat = {
-// 	make: "600",
-// 	year: "2015",
-// 	purchased: true
-// }
-
-// var carArray = [mustang, fiat];
-
-//    for (var i = 0 ; i < carArray.length ; i++){
-//    		if ( carArray[i].purchased === true){
-//    			console.log(carArray[i].make);
-//    			console.log(carArray[i].year);
-//    		}
-// }
-
-
-// carArray.forEach(function(item){
-// 	if (item.purchased === true){
-// 		console.log(item.make);
-// 		console.log(item.year);
-// 	}
-// })
-		
-	
-
-	arrUsers = [];
+	arrUsers = [{userId:'william', password:"dorsey"}];
 	var x = 0;
-	var open = 'no'
+	var open = 'no';
 
 function getAnswer(){
 		var userId = document.getElementById('userName').value;
 		var password = document.getElementById('password').value;
 		checkIfMember(userId, password);
+		open = 'no';
+	}
+
+function getAnswer1(){
+		var userId = document.getElementById('userName1').value;
+		var password = document.getElementById('password1').value;
+		ErrorMsg = 0;
+		createNewAccount(userId, password);
 	}
 
 function checkIfMember(userId, password){
 	for (var i = 0; i< arrUsers.length ; i++){
 		if (arrUsers[i]['userId'].toUpperCase() == userId.toUpperCase() && arrUsers[i]['password'].toUpperCase() == password.toUpperCase()){
 			document.querySelector('.yellow').style.backgroundColor = "green";
+			document.querySelector('.box').style.backgroundColor = "green";
+			document.querySelector('#access').style.backgroundColor = "green";
+			document.querySelector('#box').style.backgroundColor = "green";
 			document.getElementById('access').innerHTML = "ACCESS GRANTED";
 			console.log(arrUsers[i].userId);
 			console.log(userId);
@@ -83,27 +31,41 @@ function checkIfMember(userId, password){
 			
 	}
 		if (open === "no"){
-			document.getElementById('access').innerHTML = "ACCESS DENIED";
+			document.getElementById('access').innerHTML = " ACCESS DENIED";
 			document.querySelector('.yellow').style.backgroundColor = "red";
-			$(document).ready(function(){
-			var answer = prompt("you are not a member would you like to create a new account? <y> or <n>");
-					if (answer === 'Y' || answer === 'y'){
-
-						createNewAccount(userId, password);
-
-				}	
-			});
+			document.querySelector('#access').style.backgroundColor = "red";
+			document.querySelector('.box').style.backgroundColor = "red";
+			document.querySelector('#box').style.backgroundColor = "red";
 		}
 	
 }
 
 function createNewAccount(userId, password){
+	console.log(arrUsers[0]['userId']);
+	document.getElementById('access').innerHTML = " ";
 	document.querySelector('.yellow').style.backgroundColor = "yellow";
-	x = x + 1
-	a = x.toString();
-	userIdx = userId + a;
-    userIdx = new Person(userId, password);
-	arrUsers.push(userIdx);
+	document.querySelector('.box').style.backgroundColor = "yellow";
+	document.querySelector('#access').style.backgroundColor = "yellow";
+	document.querySelector('#box').style.backgroundColor = "yellow";
+	document.querySelector('#createNewAccount').style.opacity = "0";
+	for( var i = 0 ; i < arrUsers.length ; i++){
+		if (arrUsers[i]['userId'] == userId){
+			document.getElementById('access').innerHTML = " USER ID ALREADY TAKEN";	
+			ErrorMsg = 1;
+		}
+	};
+	if (password.length < 8 ){
+		document.getElementById('access').innerHTML = " PASSWORD IS TOO SHORT (must be atleast 8 characters)";
+		ErrorMsg = 1;
+	}
+
+	if (ErrorMsg != 1){
+		x = x + 1
+		a = x.toString();
+		userIdx = userId + a;
+	    userIdx = new Person(userId, password);
+		arrUsers.push(userIdx);
+	}
 }
 
 
@@ -112,8 +74,9 @@ function Person(userId, password) {
     this.password = password;
 }
 
-function reset(){
-		location.reload();
-	}
+function createNewAccountBox(){
+	document.querySelector('#createNewAccount').style.opacity = "1";
+}
+
 
 
